f7b339a4ae62465d4c8d80e26840ec0eb7b48c3d
Misha Lew <mishalew1@gmail.com>
2021-02-13 13:59:10 -0500

    added ssh copy key

---
+++ ssh-copy-id.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# copy ssh key to server
+# ssh-copy-id -i ~/.ssh/mykey user@host
--- ssh-keygen.sh
+++ ssh-keygen.sh
@@ -2,3 +2,7 @@
 
 # Creating ed25519 keys
 ssh-keygen -t ed25519 -C "misha@example.com"
+
+# RSA key for legacy systems
+# sh-keygen -t rsa -b 4096 -C "misha@example.com"
+
